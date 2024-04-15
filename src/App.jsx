843435c4eaec8f4ff11c2bbc39783f2583ed2432
ahsamology <155317740+ahsamology@users.noreply.github.com>
2024-04-15 16:06:15 +0500

    result card progress

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,17 @@ function App() {
 
   const [number1, setNumber1] = useState(0);
   const [number2, setNumber2] = useState(0);
-  const [number3, setnumber3] = useState(0)
+  const [number3, setNumber3] = useState(0);
+  const [number4, setNumber4] = useState(0);
+  const [number5, setNumber5] = useState(0);
+  const [number6, setNumber6] = useState(0);
+  const [number7, setNumber7] = useState(0);
+  const [number8, setNumber8] = useState(0);
+  const [number9, setNumber9] = useState(0);
+  const [number10, setNumber10] = useState(0);
+
   const [sum1, setSum1] = useState(0)
-
-
+  const [sum2, setSum2] = useState(0)
 
   function handleNumber1Change(event) {
     setNumber1(event.target.value)
@@ -18,86 +25,139 @@ function App() {
 
   }
 
+  function handleNumber3Change(event) {
+    setNumber3(event.target.value)
+  }
+
+  function handleNumber4Change(event) {
+    setNumber4(event.target.value)
+  }
+
+  function handleNumber5Change(event) {
+    setNumber5(event.target.value)
+  }
+
+  function handleNumber6Change(event) {
+    setNumber6(event.target.value)
+  }
+
+  function handleNumber7Change(event) {
+    setNumber7(event.target.value)
+  }
+
+  function handleNumber8Change(event) {
+    setNumber8(event.target.value)
+  }
+
+  function handleNumber9Change(event) {
+    setNumber9(event.target.value)
+  }
+
+  function handleNumber10Change(event) {
+    setNumber10(event.target.value)
+  }
+
+
   function handleSum() {
-    let calSum21 = Number(number2) - Number(number1)
-
-    setSum1(calSum21)
-
+    let calSum15 = Number(number1) + Number(number2) + Number(number3) + Number(number4) + Number(number5)
+    let calSum610 = Number(number6) + Number(number7) + Number(number8) + Number(number9) + Number(number10)
+    setSum1(calSum15)
+    setSum2(calSum610)
 
   }
 
   return (
-    <div className="container pt-20 justify-center items-center content-center">
+    <div className="container pt-20 mt-14 justify-center items-center content-center">
       <div className="relative flex justify-around font-bold text-2xl px-3 py-2 text-blue-900">
-        <div className="absolute -top-14 border rounded shadow px-3 py-2 mb-10">
-          <p >Raheel Afzal</p>
+        <div className="absolute flex gap-5 -top-20 border rounded shadow px-3 py-2 mb-10">
+          <p >Student Name</p>
+          <input type="text" className="rounded border shadow-black text-center pl-2 w-64" />
         </div>
         <div >
-        <p className="mb-10">Subject Name</p>
+          <p className="mb-10 w-80">Subject Name</p>
 
-          <p className="mb-3">English</p>
+          <p className="mb-3 w-80">English</p>
         </div>
 
         <div>
-        <p className="mb-10">Total Marks</p>
+          <p className="mb-10 w-80">Total Marks</p>
 
-          <input value={number2} type="number" onChange={handleNumber2Change} className="rounded border shadow-black pl-2" />
+          <input value={number1} type="number" onChange={handleNumber1Change} className="rounded border shadow-black pl-2" />
         </div>
         <div>
-        <p className="mb-10">Obtained Marks</p>
+          <p className="mb-10 w-80">Obtained Marks</p>
 
-          <input value={sum1} disabled className="rounded border shadow-black pl-2" />
+          <input value={number6} type="number" onChange={handleNumber6Change}  className="rounded border shadow-black pl-2" />
         </div>
       </div>
       <div className="relative flex justify-around font-bold text-2xl px-3 py-2 text-blue-900">
         <div >
-          <p className="mb-3">Urdu</p>
+          <p className="mb-3 w-80">Urdu</p>
         </div>
 
         <div>
           <input value={number2} type="number" onChange={handleNumber2Change} className="rounded border shadow-black pl-2" />
         </div>
         <div>
-          <input value={sum1} disabled className="rounded border shadow-black pl-2" />
+          <input value={number7} type="number" onChange={handleNumber7Change} className="rounded border shadow-black pl-2" />
         </div>
       </div>
       <div className="relative flex justify-around font-bold text-2xl px-3 py-2 text-blue-900">
         <div >
-          <p className="mb-3">Math</p>
+          <p className="mb-3 w-80">Math</p>
         </div>
 
         <div>
-          <input value={number2} type="number" onChange={handleNumber2Change} className="rounded border shadow-black pl-2" />
+          <input value={number3} type="number" onChange={handleNumber3Change} className="rounded border shadow-black pl-2" />
         </div>
         <div>
-          <input value={sum1} disabled className="rounded border shadow-black pl-2" />
+          <input value={number8} type="number" onChange={handleNumber8Change} className="rounded border shadow-black pl-2" />
         </div>
       </div>
       <div className="relative flex justify-around font-bold text-2xl px-3 py-2 text-blue-900">
         <div >
-          <p className="mb-3">Chemistry</p>
+          <p className="mb-3 w-80">Chemistry</p>
         </div>
 
         <div>
-          <input value={number2} type="number" onChange={handleNumber2Change} className="rounded border shadow-black pl-2" />
+          <input value={number4} type="number" onChange={handleNumber4Change} className="rounded border shadow-black pl-2" />
         </div>
         <div>
-          <input value={sum1} disabled className="rounded border shadow-black pl-2" />
+          <input value={number9} type="number" onChange={handleNumber9Change} className="rounded border shadow-black pl-2" />
         </div>
       </div>
       <div className="relative flex justify-around font-bold text-2xl px-3 py-2 text-blue-900">
         <div >
-          <p className="mb-3">Biology</p>
+          <p className="mb-3 w-80">Biology</p>
         </div>
 
         <div>
-          <input value={number2} type="number" onChange={handleNumber2Change} className="rounded border shadow-black pl-2" />
+          <input value={number5} type="number" onChange={handleNumber5Change} className="rounded border shadow-black pl-2" />
         </div>
+        <div>
+          <input value={number10} type="number" onChange={handleNumber10Change} className="rounded border shadow-black pl-2" />
+        </div>
+      </div>
+      <hr className="my-4"/>
+      <div className="relative flex justify-around font-bold text-2xl px-3 py-2 text-blue-900">
+        <div >
+          <p className="mb-3 w-80">Total Marks</p>
+        </div>
+
         <div>
           <input value={sum1} disabled className="rounded border shadow-black pl-2" />
         </div>
+        <div>
+          <input value={sum2} disabled className="rounded border shadow-black pl-2" />
+        </div>
+      </div>
+      <div className="mt-3 mb-5 w-full flex justify-center">
+      <button onClick={handleSum} className="bg-green-600 px-6 py-2 text-lg  font-semibold rounded-xl shadow text-white mx-auto ">
+        Check Result
+      </button>
       </div>
     </div>
+    
   )
 }
 
