e6247bceac74fed8d1df8072f12f7edc735c3e04
ahsamology <155317740+ahsamology@users.noreply.github.com>
2024-04-18 16:35:38 +0500

    cgpa

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ function App() {
 
   function handlePercentage(num1, num2) {
     if (num1 && num2) { 
-      let percentage = Number(num2 / num1 * 100) .toFixed(2)
+      let percentage = Number(num2 / num1) .toFixed(2)
       return percentage
     }
     
@@ -102,95 +102,71 @@ function App() {
     }
   }
 
-  
-  function overallStatus(number) {
-
-    //ternary operator
-    return number > 165 ? 'Pass' : 'Fail'
-
-
-    // if (number > 165) {
-    //   return 'Pass'
-    // }
-    // else {
-    //   return 'Fail'
-    // }
-
-  }
 
   return (
     <div className="container pt-10 justify-center items-center content-center">
       <div className=" flex w-full    mb-10 font-bold text-2xl text-blue-900 border-b-2 pb-10">
-        <div className="mx-auto">
-          <div className="flex gap-5">
+        <div className="ml-5">
             <p >Student Name</p>
-            <input type="text" className="rounded border shadow text-center pl-2 w-56" />
-          </div>
+            <input type="text" className="rounded my-3 border shadow text-center pl-2 w-56" />
+            <p >Roll No.</p>
+            <input type="text" className="rounded my-3 border shadow text-center pl-2 w-56" />
         </div>
       </div>
 
-      <div className="grid grid-cols-5 font-bold text-2xl  py-2 text-blue-900">
-        <p>Subject Name</p>
-        <p>Total Marks</p>
-        <p>Obtained Marks</p>
-        <p>Percentage %</p>
+      <div className="grid grid-cols-5 font-bold text-2xl ml-3 py-2 text-blue-900">
+        <p>Course Title</p>
+        <p className="ml-1">Credit Hours</p>
+        <p className="ml-3">Quality Points</p>
         <p>Status</p>
       </div>
 
       <div className="relative grid grid-cols-5 gap-10 font-bold text-2xl px-3 py-2 text-blue-900">
-        <p className="mb-3">English</p>
+        <p className="mb-3">Plant Pathology</p>
         <input value={number1} type="number" onChange={handleNumber1Change} className="rounded border shadow-black pl-2" />
         <input value={number6} type="number" onChange={handleNumber6Change} className="rounded border shadow-black pl-2" />
-        <input value={handlePercentage(number1, number6)}   disabled className="rounded border shadow-black pl-2" />
-
         <p className={pickColor(number1, number6)}>{checkStatus(number1, number6)}</p>
       </div>
 
       <div className="relative grid grid-cols-5 gap-10 font-bold text-2xl px-3 py-2 text-blue-900">
-        <p className="mb-3 w-56">Urdu</p>
+        <p className="mb-3 w-56">Horticulture</p>
         <input value={number2} type="number" onChange={handleNumber2Change} className="rounded border shadow-black pl-2" />
         <input value={number7} type="number" onChange={handleNumber7Change} className="rounded border shadow-black pl-2" />
-        <input value={handlePercentage(number2, number7)} disabled className="rounded border shadow-black pl-2" />
         <p className={pickColor(number2, number7)}>{checkStatus(number2, number7)}</p>
       </div>
 
       <div className="relative grid grid-cols-5 gap-10 font-bold text-2xl px-3 py-2 text-blue-900">
-        <p className="mb-3 w-56">Math</p>
+        <p className="mb-3 w-56">Plant Breeding</p>
         <input value={number3} type="number" onChange={handleNumber3Change} className="rounded border shadow-black pl-2" />
         <input value={number8} type="number" onChange={handleNumber8Change} className="rounded border shadow-black pl-2" />
-        <input value={handlePercentage(number3, number8)} disabled className="rounded border shadow-black pl-2" />
         <p className={pickColor(number3, number8)}>{checkStatus(number3, number8)}</p>
       </div>
 
       <div className="relative grid grid-cols-5 gap-10 font-bold text-2xl px-3 py-2 text-blue-900">
-        <p className="mb-3 w-56">Chemistry</p>
+        <p className="mb-3 w-56">Entomology</p>
         <input value={number4} type="number" onChange={handleNumber4Change} className="rounded border shadow-black pl-2" />
         <input value={number9} type="number" onChange={handleNumber9Change} className="rounded border shadow-black pl-2" />
-        <input value={handlePercentage(number4, number9)} disabled className="rounded border shadow-black pl-2" />
         <p className={pickColor(number4, number9)}>{checkStatus(number4, number9)}</p>
       </div>
 
       <div className="relative grid grid-cols-5 gap-10 font-bold text-2xl px-3 py-2 text-blue-900">
-        <p className="mb-3 w-56">Biology</p>
+        <p className="mb-3 w-56">Agronomy</p>
         <input value={number5} type="number" onChange={handleNumber5Change} className="rounded border shadow-black pl-2" />
         <input value={number10} type="number" onChange={handleNumber10Change} className="rounded border shadow-black pl-2" />
-        <input value={handlePercentage(number5, number10)} disabled className="rounded border shadow-black pl-2" />
         <p className={pickColor(number5, number10)}>{checkStatus(number5, number10)}</p>
       </div>
+      <hr className="my-4" />
+<div>
+<div className="relative grid grid-cols-5 gap-10 font-bold text-2xl px-3 py-2 text-blue-900">
+        <p className="mb-3 w-56">GPA</p>
+        <input value={handlePercentage(sum1, sum2)} disabled className="rounded border shadow-black pl-2" />
+      </div>
+</div>
 
       <hr className="my-4" />
-
-      <div className="relative grid grid-cols-5 gap-10 font-bold text-2xl px-3 py-2 text-blue-900">
-        <p className="mb-3 w-56">Total Marks</p>
-        <input value={sum1} disabled className="rounded border shadow-black pl-2" />
-        <input value={sum2} disabled className="rounded border shadow-black pl-2" />
-        <input value={handlePercentage(sum1, sum2)} disabled className="rounded border shadow-black pl-2" />
-        <p className={sum2 > 165 ? "text-green-500" : "text-red-500"}>{overallStatus(sum2)}</p>
-      </div>
-
-      <div className="mt-3 mb-5 w-full flex justify-center">
+      <div className="mt-3 mb-5 w-full ml-10 ">
         <button onClick={handleSum} className="bg-green-600 px-6 py-2 text-lg  font-semibold rounded-xl shadow text-white mx-auto ">
-          Check Result
+          Calculate
         </button>
       </div>
     </div>
